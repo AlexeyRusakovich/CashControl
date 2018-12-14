b50b29d9cd8559804f227bb8af099989df88cb99
AlexeyRusakovich <alexrusakovich@mail.ru>
2018-12-14 14:31:28 +0300

    Added form components on Report page. Updated transaction color.

--- CashControl/Controllers/ReportController.cs
+++ CashControl/Controllers/ReportController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using CashControl.Interfaces;
 using CashControl.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -11,9 +12,26 @@ namespace CashControl.Controllers
 {
     public class ReportController : Controller
     {
-        [Authorize]
-        public IActionResult Index()
+        private readonly ITransactionsRepository _transactions;
+        private readonly ICurrenciesRepository _currencies;
+        private readonly ICategoriesRepository _categories;
+        private readonly ITransactionTypesRepository _transactionTypes;
+
+        public ReportController(ITransactionsRepository transactionsRepository,
+                                ICurrenciesRepository currenciesRepository,
+                                ICategoriesRepository categoriesRepository,
+                                ITransactionTypesRepository transactionTypesRepository)
         {
+            _transactions = transactionsRepository;
+            _currencies = currenciesRepository;
+            _categories = categoriesRepository;
+            _transactionTypes = transactionTypesRepository;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Currencies = await _currencies.GetAll();
             return View();
         }
     }
